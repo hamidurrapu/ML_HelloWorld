788e5af9bdb61807aca5a0ec47fb0eab539e33ed
hamidurrapu <107615420+hamidurrapu@users.noreply.github.com>
2022-09-20 16:12:52 +0200

    Update pythonscript.py

--- pythonscript.py
+++ pythonscript.py
@@ -7,7 +7,7 @@ my_button = st.button("RUN Command")
 
 if my_button:
     st.title("This is Just a Simple Streamlit Code!")
-    my_text = st.text("Thank You For Using the code!")
+    my_text = st.text("Thank You!")
 
 my_text = st.text("Thank You For Using the code!")
 
